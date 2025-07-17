20d6f8673b06d99779369c378baae2c2bf9e0e6e
dago <dagoni@mail.ru>
2025-07-17 16:02:30 +0500

    fix4

---
+++ src/ru/rtk/java/Main.java
@@ -0,0 +1,23 @@
+package src.ru.rtk.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = scanner.nextLine();
+
+//        int start = 1;
+//        int end = 5;
+//
+//        for (int i = start; i <= end; i ++) {
+//            System.out.println("Привет, " + name + "!");
+//        }
+
+        System.out.println("Привет, " + name + "!");
+
+
+
+    }
+}
