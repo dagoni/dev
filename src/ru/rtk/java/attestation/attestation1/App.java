35e5da3220eb0e648307ad991b580b9b980064c0
dago <dagoni@mail.ru>
2025-07-29 19:12:13 +0500

    fix

---
+++ src/ru/rtk/java/attestation/attestation1/App.java
@@ -0,0 +1,4 @@
+package src.ru.rtk.java.attestation.attestation1;
+
+public class App {
+}
---
+++ src/ru/rtk/java/attestation/attestation1/Person.java
@@ -0,0 +1,10 @@
+package src.ru.rtk.java.attestation.attestation1;
+
+
+
+public class Person {
+
+    private String name;
+    private String many;
+
+}
